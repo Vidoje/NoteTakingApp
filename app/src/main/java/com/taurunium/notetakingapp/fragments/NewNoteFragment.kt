f4f027ad5132db9276a08a77d078379525bfff2a
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-04-23 22:41:13 +0300

    Note entity class

---
+++ app/src/main/java/com/taurunium/notetakingapp/fragments/NewNoteFragment.kt
@@ -0,0 +1,19 @@
+package com.taurunium.notetakingapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.taurunium.notetakingapp.R
+
+
+class NewNoteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_note, container, false)
+    }
+}
