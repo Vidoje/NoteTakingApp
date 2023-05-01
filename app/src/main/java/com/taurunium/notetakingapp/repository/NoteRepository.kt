840d24721893e3d9b30f2f5b143cdac5cc17398b
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-05-01 22:53:44 +0300

    Notes repository

---
+++ app/src/main/java/com/taurunium/notetakingapp/repository/NoteRepository.kt
@@ -0,0 +1,18 @@
+package com.taurunium.notetakingapp.repository
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.taurunium.notetakingapp.database.NoteDatabase
+import com.taurunium.notetakingapp.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+
+    fun getAllNotes(): LiveData<List<Note>> = db.getNoteDao().getAllNotes()
+
+    fun searchNote(query:String): LiveData<List<Note>> = db.getNoteDao().searchNote(query)
+}
