58ec8875398deb28ea6c7c44eee00de4e94feeac
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-05-14 01:52:51 +0300

    Home fragment

--- app/src/main/java/com/taurunium/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/taurunium/notetakingapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.taurunium.notetakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taurunium.notetakingapp.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/taurunium/notetakingapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/taurunium/notetakingapp/fragments/HomeFragment.kt
@@ -6,26 +6,60 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.taurunium.notetakingapp.MainActivity
 import com.taurunium.notetakingapp.R
 import com.taurunium.notetakingapp.adapter.NoteAdapter
 import com.taurunium.notetakingapp.databinding.FragmentHomeBinding
 import com.taurunium.notetakingapp.viewmodel.NoteViewModel
 
 
-class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
+class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextListener {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var notesViewModel :NoteViewModel
-    private lateinit var noteAdaper:NoteAdapter
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdaper: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).noteViewModel
+
+        setupRecyclerView()
+        binding.fabAddNote.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        noteAdaper = NoteAdapter()
+        binding.recyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setHasOptionsMenu(true)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
