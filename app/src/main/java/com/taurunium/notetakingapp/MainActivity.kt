097b5f69e2e39a974475b8116b9efb240694a23c
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-05-18 11:41:23 +0300

    Main activity

--- app/src/main/java/com/taurunium/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/taurunium/notetakingapp/MainActivity.kt
@@ -2,14 +2,30 @@ package com.taurunium.notetakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.taurunium.notetakingapp.database.NoteDatabase
+import com.taurunium.notetakingapp.databinding.ActivityMainBinding
+import com.taurunium.notetakingapp.repository.NoteRepository
 import com.taurunium.notetakingapp.viewmodel.NoteViewModel
+import com.taurunium.notetakingapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NoteViewModel::class.java)
     }
 }
